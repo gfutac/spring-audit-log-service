15fa101e1946244d739cc99502faa8aeba30c6ca
UPC\gfutac <gfutac@upchain.com>
2020-03-08 22:33:18 +0100

    Controller renamed

--- src/main/java/com/gfutac/rest/AuditLogClientController.java
+++ src/main/java/com/gfutac/rest/AuditLogClientController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/audit-log/v1", produces = MediaType.APPLICATION_JSON_VALUE)
-public class ElasticClientController {
+public class AuditLogClientController {
 
     @Autowired
     private ElasticService elasticService;
